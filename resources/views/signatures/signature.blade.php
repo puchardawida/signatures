9946ed3643dbf235681a99449065fa17643492ad
Dawid Płocharczyk <dawidplocharczyk@outlook.com>
2022-02-12 19:44:15 +0100

    [FIX] added border none to signature blade

--- resources/views/signatures/signature.blade.php
+++ resources/views/signatures/signature.blade.php
@@ -8,29 +8,29 @@
 
     <br><br>
     <div id="signature">
-        <table  style="font-family: arial, sans-serif; font-size: 10px; border-collapse: collapse; color: #2b2a29;">
+        <table  style="font-family: arial, sans-serif; font-size: 10px; border-collapse: collapse; color: #2b2a29; border: none;">
             <tr>
                 <td style="padding-bottom: 10px;">
-                    <span style="font-size: 12px; font-weight: 400px; font-family: arial;">
+                    <span style="font-size: 12px; font-weight: 400px; font-family: arial; border: none;">
                         {{$greeting->name}},
                     </span>
                 </td>
             </tr>
             <tr>
-                <td style="border-top: 1px solid #000000; padding: 0; margin:0;"></td>
+                <td style="border: none; border-top: 1px solid #000000; padding: 0; margin:0;"></td>
             </tr>
             <tr>
-                <td style="padding:0; margin:0;">
+                <td style="border: none; padding:0; margin:0;">
                     <table style="padding:0; margin:0;">
                         <tr>
-                            <td style="padding-top: 25px; margin-top: 0px; width: 300px;">
+                            <td style="border: none; padding-top: 25px; margin-top: 0px; width: 300px;">
                                 <table>
                                     <tr>
-                                        <td>
+                                        <td style="border: none;">
                                             <table>
                                                 <tr>
                                                     @if($request->show_photo)
-                                                    <td>
+                                                    <td style="border: none;">
                                                         <img
                                                             width="75"
                                                             height="75"
@@ -39,10 +39,10 @@
                                                         >
                                                     </td>
                                                     @endif
-                                                    <td @if($request->show_photo) style="padding-left: 10px;" @endif>
+                                                    <td @if($request->show_photo) style="padding-left: 10px; border: none;" @endif>
                                                         <table>
                                                             <tr>
-                                                                <td style="font-weight: 700; font-size: 14px; text-transform: uppercase;">
+                                                                <td style="font-weight: 700; font-size: 14px; text-transform: uppercase; border: none;">
                                                                     {{$request->name}}
                                                                 </td>
                                                             </tr>
@@ -50,7 +50,7 @@
                                                                 <td style="border-top: 1px solid #e10123;"></td>
                                                             </tr>
                                                             <tr>
-                                                                <td style="font-weight: 700; font-size: 11px; text-transform: uppercase;">
+                                                                <td style="font-weight: 700; font-size: 11px; text-transform: uppercase; border: none;">
                                                                     {{$request->position}}
                                                                     <br>															
                                                                     {{$request->department}}
@@ -64,14 +64,14 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td style="font-size: 12px;">
+                                        <td style="font-size: 12px; border: none;">
                                             <table cellpadding="0" cellspacing="0">
                                                 @foreach($contacts as $contact)
                                                 <tr cellpadding="0" cellspacing="0" style="padding; 0; margin: 0;">
-                                                    <td cellpadding="0" cellspacing="0" style="padding; 0; margin: 0;">
+                                                    <td cellpadding="0" cellspacing="0" style="padding; 0; margin: 0; border: none;">
                                                         <img width="20" height="20" style="width: 20px; height: 20px;" src="{{$contact['icon']}}">                                                       
                                                     </td>
-                                                    <td cellpadding="0" cellspacing="0" style="font-size: 11px; padding; 0; margin: 0; padding-left: 5px; font-weight: 400;">
+                                                    <td cellpadding="0" cellspacing="0" style="font-size: 11px; padding; 0; margin: 0; padding-left: 5px; font-weight: 400; border: none;">
                                                         <a href="{{$contact['url']}}" style="text-decoration: none; color: #000000;">
                                                             {{$contact['value']}}
                                                         </a>
@@ -83,7 +83,7 @@
                                     </tr>
                                 </table>
                             </td>
-                            <td style="padding-top: 0px; margin-top: 0px; vertical-align:top; width: 500px; text-align: right;">
+                            <td style="padding-top: 0px; margin-top: 0px; vertical-align:top; width: 500px; text-align: right; border: none;">
                                 <img width="500" height="215" style="width: 500px; height: 215px;" src="{{$banner_url}}">
                             </td>
                         </tr>
@@ -91,10 +91,10 @@
                 </td>
             </tr>
             <tr>
-                <td>
+                <td style="border: none;">
                     <table style="width: 800px;">
                         <tr>
-                            <td style="width: 600px; font-size: 11px;">
+                            <td style="width: 600px; font-size: 11px; border: none;">
                                 <div style="padding-top: 2px; padding-bottom: 2px;">
                                     <span style="font-weight: 600; padding-right: 4px;">GRUPA MAKURAT</span>
                                     <span style="color:#e10123; font-weight: 700;">|</span>
@@ -124,7 +124,7 @@
                                     @endif
                                 @endforeach
 
-                                <div style="padding-top: 2px; padding-bottom: 2px;">
+                                <div style="padding-top: 2px; padding-bottom: 2px; border: none;">
                                     <span style="padding-right: 4px;">NIP {{$company->nip}}</span>
                                     <span style="color:#e10123; font-weight: 700;">|</span>
                                     <span style="padding-right: 4px; padding-left: 4px;">REGON {{$company->regon}}</span>
@@ -132,7 +132,7 @@
                                     <span style="padding-left: 4px;">KRS {{$company->krs}}</span>
                                 </div>
                             </td>
-                            <td style="width: 200px; text-align: right;" width="200">
+                            <td style="width: 200px; text-align: right; border: none;" width="200">
                                 <a href="{{$company->google_url}}">
                                     <img width="200" height="35" tyle="width: 200px; height: 35px;" src="{{$google_image}}">
                                 </a>
